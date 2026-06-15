d5101215c47c9cfeae505bc96a7fd3fb601b5614
Archanapatel9 <archanapatel6261@gmail.com>
2026-06-15 23:27:34 +0530

    my string

---
+++ mystring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class mystring {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your name:");
+    String name = sc.nextLine();
+    System.out.println("Welcome "+ name);
+    } 
+}
